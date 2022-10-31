d682205fe79b1d5953cbca95b65eb847436d0960
Vergara <jvv10.vergara@gmail.com>
2022-10-31 14:10:37 -0300

    modified:   bin/Main.class 	new file:   bin/br/com/dio/desafio/dominio/Bootcamp.class 	new file:   bin/br/com/dio/desafio/dominio/Conteudo.class 	modified:   bin/br/com/dio/desafio/dominio/Curso.class 	new file:   bin/br/com/dio/desafio/dominio/Dev.class 	modified:   bin/br/com/dio/desafio/dominio/Mentoria.class 	modified:   src/Main.java 	new file:   src/br/com/dio/desafio/dominio/Bootcamp.java 	new file:   src/br/com/dio/desafio/dominio/Conteudo.java 	modified:   src/br/com/dio/desafio/dominio/Curso.java 	new file:   src/br/com/dio/desafio/dominio/Dev.java 	modified:   src/br/com/dio/desafio/dominio/Mentoria.java
    Atualizacao

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.time.LocalDate;
+import java.util.*;
 
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -15,14 +18,24 @@ public class Main {
         curso2.setDescricao("descrição curso js");
         curso2.setCargaHoraria(8);
 
+        Conteudo conteudo = new Curso();
+        Conteudo conteudo1 = new Mentoria();
+
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
     }
 }
